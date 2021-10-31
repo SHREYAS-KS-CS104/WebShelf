36fbd21d75b0fd5fc5c5d8f2e95990580a42f123
SHREYAS-KS-CS104 <shreyasks.cs18@bmsce.ac.in>
2021-11-01 00:04:40 +0530

    Set up the Web Shelf Backend project

---
+++ WebShelfParent/WebShelfBackEnd/src/main/java/com/webshelf/admin/WebShelfBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.webshelf.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShelfBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebShelfBackEndApplication.class, args);
+	}
+
+}
