7d854e2b46e2de7a33ab86c0f8e44df351dd8696
SHREYAS-KS-CS104 <shreyasks.cs18@bmsce.ac.in>
2021-11-01 01:14:20 +0530

    Set up the Web Shelf Frontend project

---
+++ WebShelfParent/WebShelfFrontEnd/src/test/java/com/webshelf/WebShelfFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShelfFrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
